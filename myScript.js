0f51621ad99ac2ab367fbd84b1e9839200ba34e6
Kanwarjit Rupyal <kanwar.rupyal@gmail.com>
2018-11-13 13:59:02 -0600

    Dice Selection added/made numSd a Global variable-approved by Nevin

--- myScript.js
+++ myScript.js
@@ -1,17 +1,59 @@
+
+let numSd;
 //DiceGame
 function rollDice(){
-	let numSd;
-	let roll = Math.random()*numSd;
-	roll = Math.ceil(roll);
+	roll = Math.ceil(Math.random()*numSd);
 	return roll; 
 }
 
-//Sides of Dice
-function diceSides(num){
-	num=prompt("Enter the no. of sides of the dice you want to use:");
-	let z=rollDice();
-	console.log(z);
+
+//Select Dice
+function diceSelect(){
+	let userInput=prompt("4-Sided(1), 6-Sided(2), 8-Sided(3), 10-Sided(4), 12-Sided(5), 20-Sided(6)");
+	switch(userInput){
+		case "1":
+			numSd=4;
+			console.log("4 Sided Dice Selected")
+			break;
+		case "2":
+			numSd=6;
+			console.log("6 Sided Dice Selected")
+			break;
+		case "3":
+			numSd=8;
+			console.log("8 Sided Dice Selected")
+			break;
+		case "4":
+			numSd=10;
+			console.log("10 Sided Dice Selected")
+			break;
+		case "5":
+			numSd=12;
+			console.log("12 Sided Dice Selected")
+			break;
+		case "6":
+			numSd=20;
+			console.log("20 Sided Dice Selected")
+			break;
+		default:
+			break;
+	}
 }
+
+function p1(){
+	let p1Roll = rollDice(numSd);
+	console.log(p1Roll);
+}
+
+function p2(){
+	let p2Roll = rollDice(numSd);
+	console.log(p2Roll);
+}
+
+
+
+
+
 
 
 //function dispPts(){
